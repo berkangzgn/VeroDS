10a0acebd13ffac91e7de101a399f10395480eef
Berkan <bgezgin@protonmail.com>
2023-04-06 16:06:29 +0300

    Initial Commit

---
+++ VeroDS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VeroDS
+//
+//  Created by Berkan Gezgin on 6.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
